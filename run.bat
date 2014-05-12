7747111231db986d74dc13d6a9c1f5871236ce7f
jeethualex <jeethualex@yahoo.com>
2014-05-12 21:42:28 +0400

    Stable Commit

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-cd C:\Users\jeeth_000\git\spring-maven-gep-example
+cd C:\Users\jeeth_000\git\spring-maven-gep-example-1
 set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_51
 mvn appengine:update
 pause
