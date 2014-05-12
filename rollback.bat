09ee441b5d77711a89fa5c05d177095db527e89a
jeethualex <jeethualex@yahoo.com>
2014-05-12 22:25:47 +0400

    Final Commit

--- rollback.bat
+++ rollback.bat
@@ -1,2 +1,2 @@
 cd C:\Program Files\eclipse\plugins\com.google.appengine.eclipse.sdkbundle_1.9.2\appengine-java-sdk-1.9.2\bin
-appcfg.cmd rollback "C:\Users\jeeth_000\workspace\spring-maven-gep-example\src\main\webapp"
+appcfg.cmd rollback "C:\Users\jeeth_000\git\spring-maven-gep-example-1\src\main\webapp"
